390a24e7e436d550e064b83d39a6e61c60e28b57
clover <ThreeLeafClover9@gmail.com>
2022-12-29 20:47:40 +0900

    Build CoffeeOrderWebApplication

---
+++ src/main/java/com/codestates/CoffeeOrderWeb/CoffeeOrderWebApplication.java
@@ -0,0 +1,13 @@
+package com.codestates.CoffeeOrderWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeOrderWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeOrderWebApplication.class, args);
+	}
+
+}
