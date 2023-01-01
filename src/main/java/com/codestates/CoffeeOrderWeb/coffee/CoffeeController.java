6477a5e26e19ecff8ca1f837749b82a8b1427978
clover <ThreeLeafClover9@gmail.com>
2023-01-01 15:41:08 +0900

    Refactor ResponseEntity

--- src/main/java/com/codestates/CoffeeOrderWeb/coffee/CoffeeController.java
+++ src/main/java/com/codestates/CoffeeOrderWeb/coffee/CoffeeController.java
@@ -1,41 +1,39 @@
 package com.codestates.CoffeeOrderWeb.coffee;
 
-import org.springframework.http.MediaType;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
-@RequestMapping(value = "/v1/coffees", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping("/v1/coffees")
 public class CoffeeController {
     @PostMapping
-    public String postCoffee(@RequestParam("engName") String engName,
-                             @RequestParam("korName") String korName,
-                             @RequestParam("price") int price) {
-        System.out.println("engName = " + engName);
-        System.out.println("korName = " + korName);
-        System.out.println("price = " + price);
-
-        String response =
-                "{\"" +
-                    "engName\":\"" + engName + "\"," +
-                    "\"korName\":\"" + korName + "\",\"" +
-                    "price\":" + price +
-                "}";
-        return response;
+    public ResponseEntity postCoffee(@RequestParam("engName") String engName,
+                                     @RequestParam("korName") String korName,
+                                     @RequestParam("price") int price) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("engName", engName);
+        map.put("korName", korName);
+        map.put("price", price);
+        return new ResponseEntity<>(map, HttpStatus.CREATED);
     }
 
     @GetMapping("/{coffee-id}")
-    public String getCoffee(@PathVariable("coffee-id") long coffeeId) {
+    public ResponseEntity getCoffee(@PathVariable("coffee-id") long coffeeId) {
         System.out.println("coffeeId = " + coffeeId);
 
         // not implementation
-        return null;
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @GetMapping
-    public String getCoffees() {
+    public ResponseEntity getCoffees() {
         System.out.println("get Coffees");
 
         // not implementation
-        return null;
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
--- src/main/java/com/codestates/CoffeeOrderWeb/order/OrderController.java
+++ src/main/java/com/codestates/CoffeeOrderWeb/order/OrderController.java
@@ -1,38 +1,37 @@
 package com.codestates.CoffeeOrderWeb.order;
 
-import org.springframework.http.MediaType;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
-@RequestMapping(value = "/v1/orders", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping("/v1/orders")
 public class OrderController {
     @PostMapping
-    public String postOrder(@RequestParam("memberId") long memberId,
-                            @RequestParam("coffeeId") long coffeeId) {
-        System.out.println("memberId = " + memberId);
-        System.out.println("coffeeId = " + coffeeId);
-
-        String response =
-                "{\"" +
-                    "memberId\":" + memberId + "," +
-                    "\"coffeeId\":" + coffeeId +
-                "}";
-        return response;
+    public ResponseEntity postOrder(@RequestParam("memberId") long memberId,
+                                    @RequestParam("coffeeId") long coffeeId) {
+        Map<String, Long> map = new HashMap<>();
+        map.put("memberId", memberId);
+        map.put("coffeeId", coffeeId);
+        return new ResponseEntity<>(map, HttpStatus.CREATED);
     }
 
     @GetMapping("/{order-id}")
-    public String getOrder(@PathVariable("order-id") long orderId) {
+    public ResponseEntity getOrder(@PathVariable("order-id") long orderId) {
         System.out.println("orderId = " + orderId);
 
         // not implementation
-        return null;
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @GetMapping
-    public String getOrders() {
+    public ResponseEntity getOrders() {
         System.out.println("get Orders");
 
         // not implementation
-        return null;
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
