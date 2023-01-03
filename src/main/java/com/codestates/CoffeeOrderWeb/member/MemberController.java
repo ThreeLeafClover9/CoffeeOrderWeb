ebeacacbdf5a36f614d20c866c8a1ef9de66f6e1
clover <ThreeLeafClover9@gmail.com>
2023-01-03 23:37:43 +0900

    Feat validation

--- src/main/java/com/codestates/CoffeeOrderWeb/member/MemberController.java
+++ src/main/java/com/codestates/CoffeeOrderWeb/member/MemberController.java
@@ -2,21 +2,25 @@ package com.codestates.CoffeeOrderWeb.member;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
 
 @RestController
 @RequestMapping("/v1/members")
+@Validated
 public class MemberController {
     @PostMapping
-    public ResponseEntity postMember(@RequestBody MemberPostDto memberPostDto) {
+    public ResponseEntity postMember(@Valid @RequestBody MemberPostDto memberPostDto) {
         return new ResponseEntity<>(memberPostDto, HttpStatus.CREATED);
     }
 
     @PatchMapping("/{member-id}")
-    public ResponseEntity patchMember(@PathVariable("member-id") long memberId,
-                                      @RequestBody MemberPatchDto memberPatchDto) {
+    public ResponseEntity patchMember(@PathVariable("member-id") @Min(1) long memberId,
+                                      @Valid @RequestBody MemberPatchDto memberPatchDto) {
         memberPatchDto.setMemberId(memberId);
-        memberPatchDto.setName("홍길동");
         return new ResponseEntity<>(memberPatchDto, HttpStatus.OK);
     }
 
