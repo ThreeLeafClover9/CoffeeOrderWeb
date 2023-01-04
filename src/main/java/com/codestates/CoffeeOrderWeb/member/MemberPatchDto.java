5d263cde81622ac7703b63ab8f1f98f02a02dd3a
clover <ThreeLeafClover9@gmail.com>
2023-01-04 11:06:56 +0900

    Refactor custom validator

--- src/main/java/com/codestates/CoffeeOrderWeb/member/MemberPatchDto.java
+++ src/main/java/com/codestates/CoffeeOrderWeb/member/MemberPatchDto.java
@@ -1,5 +1,6 @@
 package com.codestates.CoffeeOrderWeb.member;
 
+import com.codestates.CoffeeOrderWeb.validator.NotSpace;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,7 +10,7 @@ import javax.validation.constraints.Pattern;
 @Setter
 public class MemberPatchDto {
     private long memberId;
-    @Pattern(regexp = "^\\S+(\\s?\\S+)*$", message = "회원 이름은 공백이 아니어야 합니다.")
+    @NotSpace(message = "회원 이름은 공백이 아니어야 합니다.")
     private String name;
     @Pattern(regexp = "^010-\\d{3,4}-\\d{4}$",
             message = "휴대폰 번호는 010으로 시작하는 11자리 숫자와 '-'로 구성되어야 합니다.")
---
+++ src/main/java/com/codestates/CoffeeOrderWeb/validator/NotSpace.java
@@ -0,0 +1,19 @@
+package com.codestates.CoffeeOrderWeb.validator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Constraint(validatedBy = NotSpaceValidator.class)
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface NotSpace {
+    String message() default "공백이 아니어야 합니다";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
