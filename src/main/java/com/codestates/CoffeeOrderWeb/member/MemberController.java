9a593540169bcf68cf0c1320bc0c93ce83ac684d
clover <ThreeLeafClover9@gmail.com>
2023-01-05 12:16:15 +0900

    Feat service connection DI

--- src/main/java/com/codestates/CoffeeOrderWeb/member/MemberController.java
+++ src/main/java/com/codestates/CoffeeOrderWeb/member/MemberController.java
@@ -7,41 +7,54 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.Positive;
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/members")
 @Validated
 public class MemberController {
+    private final MemberService memberService;
+
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
     @PostMapping
     public ResponseEntity postMember(@Valid @RequestBody MemberPostDto memberPostDto) {
-        return new ResponseEntity<>(memberPostDto, HttpStatus.CREATED);
+        Member member = new Member();
+        member.setEmail(memberPostDto.getEmail());
+        member.setName(memberPostDto.getName());
+        member.setPhone(memberPostDto.getPhone());
+        Member response = memberService.createMember(member);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
     @PatchMapping("/{member-id}")
     public ResponseEntity patchMember(@PathVariable("member-id") @Positive long memberId,
                                       @Valid @RequestBody MemberPatchDto memberPatchDto) {
-        memberPatchDto.setMemberId(memberId);
-        return new ResponseEntity<>(memberPatchDto, HttpStatus.OK);
+        Member member = new Member();
+        member.setMemberId(memberId);
+        member.setName(memberPatchDto.getName());
+        member.setPhone(memberPatchDto.getPhone());
+        Member response = memberService.updateMember(member);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @GetMapping("/{member-id}")
     public ResponseEntity getMember(@PathVariable("member-id") @Positive long memberId) {
-        System.out.println("memberId = " + memberId);
-        // not implementation
-        return new ResponseEntity<>(HttpStatus.OK);
+        Member response = memberService.findMember(memberId);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @GetMapping
     public ResponseEntity getMembers() {
-        System.out.println("get Members");
-        // not implementation
-        return new ResponseEntity<>(HttpStatus.OK);
+        List<Member> response = memberService.findMembers();
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @DeleteMapping("/{member-id}")
     public ResponseEntity deleteMember(@PathVariable("member-id") @Positive long memberId) {
-        System.out.println("memberId = " + memberId);
-        // No need business logic
+        memberService.deleteMember(memberId);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
